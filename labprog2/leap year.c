3160632b718b010bf9ed902abd2ddd9f78a7da1c
kong14ee <kong14ee@cmrit.ac.in>
2016-09-22 19:59:44 +0530

    Update and rename main.c to leap year.c

---
+++ labprog2/leap year.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+  {
+  int year;
+  printf("enter the year to check: \n");
+  scanf("%d",&year);
+   if (year%400==0)  {
+   printf("it is a leap year\n");
+   }
+   if (year%4==0)  }
+   printf("it is a leap year\n");
+   }
+   else {
+   printf("it is not a leap year\n");
+   }
+   return 0;
+   }
