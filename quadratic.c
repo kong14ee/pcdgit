70759128d34531624785bfcdc41b5f9a4c21602b
kong14ee <kong14ee@cmrit.ac.in>
2016-09-22 19:36:08 +0530

    Update and rename labprog1/main.c to quadratic.c

---
+++ quadratic.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+     double a,b,c,d,f,r1,r2,rr,ir;
+     printf("enter the coeffiicient of the equation \n");
+    scanf("%lf%lf%lf",&a,&b,&c);
+    d=b*b-4*c ;
+    if(d==0)
+        {
+           r1=r2=-b/2*a;
+           printf("roots are equal r1=r2 %lf\n",r1);
+        }
+        else if(d>0)
+        {
+          e=sqrt(d);
+          r1=(-b+e)/2*a ;
+          r2=(-b-e)/2*a ;
+          printf("roots are real r1=%lf,r2=%lf\n" ,r1 , r2);
+        }
+        else
+        {
+          f=sqrt(-d);
+          rr=(-b/2*a);
+          ir=(f/2*a);
+          printf("roots are complex r1=%lf+i%lf , r2=%lf+i%lf \n",rr,ir);
+}
+return 0;
+}
+  
+        }
+        return 0;
+}
